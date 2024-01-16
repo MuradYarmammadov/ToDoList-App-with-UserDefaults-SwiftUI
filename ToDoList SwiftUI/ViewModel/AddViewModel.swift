2a03d852793e419e8017abe727353e009353e72c
Murad Yarmammadov <yarmamedov.murad01@gmail.com>
2024-01-16 23:55:56 +0100

    View and ViewModels finished, UserDefaults configurations

---
+++ ToDoList SwiftUI/ViewModel/AddViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddViewModel.swift
+//  ToDoList SwiftUI
+//
+//  Created by Murad Yarmamedov on 16.01.24.
+//
+
+import Foundation
---
+++ ToDoList SwiftUI/ViewModel/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ToDoList SwiftUI
+//
+//  Created by Murad Yarmamedov on 12.01.24.
+//
+
+import Foundation
